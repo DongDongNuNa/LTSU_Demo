009648a6739710ff162e20bde6d08a630ba2a356
DongDongNuNa <114869963+DongDongNuNa@users.noreply.github.com>
2023-12-05 14:15:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,10 @@ st.header("안뇽!")
 st.title("저녁 메뉴 추천 받습니다")
 
 
-#st.button('Hit me')
+st.button('Hit me')
+st.checkbox('Check me out')
+st.slider('Slide me', min_value=0, max_value=10)
+
+st.number_input('Enter a number')
+st.text_area('Area for textual entry')
+st.color_picker('Pick a color')
