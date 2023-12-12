1e1ceeb02fbeab6028bc7bf50f566daa9d9f123e
DongDongNuNa <114869963+DongDongNuNa@users.noreply.github.com>
2023-12-12 14:26:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import datetime
+import pandas as pd
 
 @st.cache_data()
 def load_df():
@@ -21,19 +21,17 @@ def load_df():
 
     return df, survival_options, p_class_options, sex_options, embark_options, min_fare, max_fare, min_age, max_age
 
-
 def check_rows(column, options):
     return res.loc[res[column].isin(options)]
 
-st.title("Demo dataframe query app")
+st.title("Demo DataFrame Query App")
 
 df, survival_options, p_class_options, sex_options, embark_options, min_fare, max_fare, min_age, max_age = load_df()
 res = df
 
-name_query = st.text_input("String match for name")
+name_query = st.text_input("String match for Name")
 
 cols = st.columns(4)
-
 survival = cols[0].multiselect("Survived", survival_options)
 p_class = cols[1].multiselect("Passenger Class", p_class_options)
 sex = cols[2].multiselect("Sex", sex_options)
@@ -41,12 +39,11 @@ embark = cols[3].multiselect("Embarked", embark_options)
 
 range_cols = st.columns(3)
 min_fare_range, max_fare_range = range_cols[0].slider("Lowest Fare", float(min_fare), float(max_fare),
-                                                [float(min_fare), float(max_fare)])
+                                                                                            [float(min_fare), float(max_fare)])
 min_age_range, max_age_range = range_cols[2].slider("Lowest Age", float(min_age), float(max_age),
-                                                [float(min_age), float(max_age)])
+                                                                                            [float(min_age), float(max_age)])
 
-
-if name_query != "":
+if name_query !="":
     res = res.loc[res.Name.str.contains(name_query)]
 
 if survival:
@@ -58,21 +55,16 @@ if p_class:
 if sex:
     res = check_rows("Sex", sex)
 
-if embarked:
-    res = check_rows("Embarked", embarked)
-
+if embark:
+    res = check_rows("Embarked", embark)
 
 if range_cols[0].checkbox("Use Fare Range"):
-    res = res.loc[(res.Fare > min_fare_range) & (res.Fare < max_fare_range)]
-
-
-
+    res = res.loc[(res.Age>min_fare_range)&(res.Age<max_fare_range)]
 if range_cols[2].checkbox("Use Age Range"):
-    res = res.loc[(res.Age > min_age_range) & (res.Age < max_age_range)]
+    res = res.loc[(res.Age>min_age_range)&(res.Age<max_age_range)]
 
-removal_columns = st.multiselect("Select volumns to remove", df.columns.tolist())
-
+removal_columns = st.multiselect("Select Columns to Remove", df.columns.tolist())
 for column in removal_columns:
-    res = res.drop(column, axis = 1)
+    res = res.drop(column, axis=1)
 
 st.write(res)
