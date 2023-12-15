1f6f4119b5ce0b69ea6e1651d41cbb3e73e51237
DongDongNuNa <114869963+DongDongNuNa@users.noreply.github.com>
2023-12-16 03:16:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-
-
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
 # Load the Titanic dataset from CSV file
-titanic_data = pd.read_csv("titanic.csv")
+titanic_data = pd.read_csv('data/titanic.csv')
 
 # Group the data by 'Sex' and 'Survived', and count the number of occurrences
 grouped_data = titanic_data.groupby(['Sex', 'Survived']).size().unstack()
